f4cfac345d9f64719e0b9b3a953b01389a4f7f0e
Your Name <you@example.com>
2024-12-27 17:26:15 -0600

    Update Chat and ChatBody

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -205,29 +205,12 @@ export default function Chat({ thread, isVisible, onClick }) {
         
         <div className={`${isVisible ? "translate-x-0" : "translate-x-full"} 
         lg:translate-x-0 relative z-10 w-full lg:static lg:w-auto transition border-x border-gray-100 bg-white flex flex-col h-screen`}>
-            {/* Header */}
+        //     {/* Header */}
             <div className="flex-none flex items-center h-24 p-6 border-b border-gray-100">
-                <button
-                    onClick={onClick}
-                    className="flex items-center justify-center mr-6 lg:hidden"
-                    type="button"
-                    aria-label="Back"
-                >
-                    <ArrowLeftIcon className="w-6 h-6" />
-                </button>
-                {/* <Avatar
-                    textImage={currentThread.shortName}
-                    isOnline={currentThread.isOnline}
-                    srcImage={currentThread.avatar}
-                />
-                <div className="flex flex-col ml-4">
-                    <span className="font-bold text-xl">{currentThread.name}</span>
-                    <span className="text-xs text-gray-400">New conversation</span>
-                </div> */}
                 <SuggestedQuestions onQuestionClick={handleQuestionClick} />
             </div>
 
-            <div className="flex flex-col flex-grow overflow-hidden">
+            <div className="flex flex-col flex-grow overflow-hidden h-screen">
             {/* Chat Body with status */}         
                 <ChatBody 
                     thread={currentThread}
